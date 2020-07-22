57c8eeb73ab25080d92a98c2d88a9b29f5452b1a
unknown <general@simplexwebsites.com>
2020-07-22 12:53:42 -0400

    all pimped out with comments!

--- linked_list_cpp/main.cpp
+++ linked_list_cpp/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 struct Node {
     int value;
-    struct Node* next = nullptr; //create the pointer to the next Node
+    struct Node* next; //create the pointer to the next Node
 };
 
 class LL {
@@ -13,27 +13,27 @@ private:
     struct Node* head; //create the pointer to head Node.
 public:
     int add(int value) {
-        if (head == NULL) {
-            this->head = (struct Node*)malloc(sizeof(struct Node));
-            this->head->value = value;
+        if (head == nullptr) { //check if the pointer to the head node is null
+            this->head = (struct Node*)malloc(sizeof(struct Node)); //allocate memory for the pointer to the head node, and allocate memory for the actual node in the heap.
+            this->head->value = value; //set the value of the head node to the value passed.
         }
         else {
-            struct Node* node = this->head;
-            while (node->next != NULL) //0xCDCDCDCD (only on MSVS, not g++)
+            struct Node* node = this->head; //create a pointer to the pointer of the head node 
+            while (node->next != nullptr) //check if the next pointer to a node is null
             {
-                node = node->next;
+                node = node->next; //if the next pointer is null, set node to the next node. (to keep looking).
             }
-            node->next = (struct Node*)malloc(sizeof(struct Node));
-            node->next->value = value;
+            node->next = (struct Node*)malloc(sizeof(struct Node)); //allocate memory for both the next node's pointer, and the actual node in the heap.
+            node->next->value = value; //set the value of the next node to the value passed.
         }
         return 0;
     }
 
     int show() {
-        struct Node* node = this->head;
-        while (node != NULL) { 
-            cout << node->value << endl;
-            node = node->next;
+        struct Node* node = this->head; //make a pointer to the head node
+        while (node != nullptr) { //check if the pointer to the node is not null
+            cout << node->value << endl; //print the value of the node
+            node = node->next; //set the node to the next node. (to keep looking).
         }
         return 0;
     }
