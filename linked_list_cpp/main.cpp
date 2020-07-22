419993783a4bade56022587502d7abdb61eb1d62
unknown <general@simplexwebsites.com>
2020-07-22 17:31:43 -0400

    Free memory when it's not being used

--- linked_list_cpp/main.cpp
+++ linked_list_cpp/main.cpp
@@ -16,8 +16,7 @@ public:
         if (head == nullptr) { //check if the pointer to the head node is null
             this->head = (struct Node*)malloc(sizeof(struct Node)); //allocate memory for the pointer to the head node, and allocate memory for the actual node in the heap.
             this->head->value = value; //set the value of the head node to the value passed.
-        }
-        else {
+        } else {
             struct Node* node = this->head; //create a pointer to the pointer of the head node 
             while (node->next != nullptr) //check if the next pointer to a node is null
             {
@@ -29,11 +28,16 @@ public:
         return 0;
     }
 
+    int add_after(int index, int value) {
+        struct Node* node = this->head; //store a pointer to the head node in variable node.
+    }
+
     int show_one_value(int index) {
         struct Node* node = this->head; //store a pointer to the head node in variable node.
         for (int i = 0; node->next != nullptr; i++) { //for loop that increments i by 1 if the pointer to the next node isn't null.
             if (i == index) {  //check if i is equal to the index passed
                 cout << node->value << endl;; //if it is, print the value of the node
+                free(node);
                 return 0;
             } else {
                 node = node->next; //if it isn't, set node to the next node
@@ -47,6 +51,7 @@ public:
             cout << node->value << endl; //print the value of the node
             node = node->next; //set the node to the next node. (to keep looking).
         }
+        free(node);
         return 0;
     }
 
@@ -58,4 +63,5 @@ int main() {
     linked_list->add(2);
     linked_list->add(10);
     linked_list->show();
+    linked_list->show_one_value(2);
 }
